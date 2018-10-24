bbe66fa0a2438949213e0b83ec52150196c6feea
Pye <sabuy3rd@gmail.com>
2018-10-24 16:06:36 +0700

    24-Oct-18
    Signed-off-by: Pye <sabuy3rd@gmail.com>

--- app/src/main/java/biophics/mdot/Fragment/HomeFragment.java
+++ app/src/main/java/biophics/mdot/Fragment/HomeFragment.java
@@ -7,6 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.ebanx.swipebtn.OnStateChangeListener;
+import com.ebanx.swipebtn.SwipeButton;
 
 import biophics.mdot.R;
 
@@ -65,7 +69,16 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        SwipeButton swipeButton = (SwipeButton) v.findViewById(R.id.swipe_btn);
+        swipeButton.setOnStateChangeListener(new OnStateChangeListener() {
+            @Override
+            public void onStateChange(boolean active) {
+                Toast.makeText(getActivity(), "True.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -100,4 +113,5 @@ public class HomeFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
 }
